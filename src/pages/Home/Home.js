8192a6beca41ac44a34647474c760544681e5dfb
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2024-02-22 21:21:51 +0200

    22

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,17 +12,6 @@ const Home = () => {
   return (
     <section className="Home" id="Home">
       <div className="Home__container container">
-        <div className="Home__links">
-          <a href="https://www.linkedin.com/in/walid-m0stafa-">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a href="https://github.com/WalidMostafa30">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a href="https://www.facebook.com/profile.php?id=100011538554307&mibextid=ZbWKwL">
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-        </div>
         <div className="Home_info">
           <p>Hello I'm</p>
           <h1>Walid Mostafa 🖐</h1>
